e214dc9aec8332f10649ba1cea2e719bbe5d5d32
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-09-30 20:26:13 +0900

    Add extension method for simplify the code

---
+++ StrikesLibrary.Test/UniqueKeyPolicyExtensionTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Documents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+namespace StrikesLibrary.Test
+{
+    public class UniqueKeyPolicyExtensionTest
+    {
+        [Fact]
+        public void UniqKey_Add_Normal_Case()
+        {
+            var ExpectedFirstKey = "/foo";
+            var ExpectedSecondKey = "/bar";
+            var NotExpectedThriedKey = "/baz";
+
+            var policy = new UniqueKeyPolicy();
+            policy.AddUniqueKey(ExpectedFirstKey);
+            policy.AddUniqueKey(ExpectedSecondKey);
+
+            var keys = policy.UniqueKeys.ToList();
+            Assert.Equal(ExpectedFirstKey, keys[0].Paths[0]);
+            Assert.Equal(ExpectedSecondKey, keys[1].Paths[0]);
+            Assert.Equal(2, keys.Count());
+        }
+
+        private UniqueKey getUniqueKey(string path)
+        {
+            var key = new UniqueKey();
+            key.Paths.Add(path);
+            return key;
+        }
+            
+    }
+}
---
+++ StrikesLibrary/UniqueKeyPolicyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Documents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrikesLibrary
+{
+    public static class UniqueKeyPolicyExtensions
+    {
+        public static void AddUniqueKey(this UniqueKeyPolicy policy, string path)
+        {
+            var keys = policy.UniqueKeys;
+            var uniqueKey = new UniqueKey();
+            var paths = uniqueKey.Paths;
+            paths.Add(path);
+            policy.UniqueKeys.Add(uniqueKey);
+        }
+    }
+}
